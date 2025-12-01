64182edddd85abb27d74437a42aedd314f5c5e5f
KoniPN <kong.18th@gmail.com>
2025-12-01 15:55:05 +0700

    Add initial SQL script to create users_test table and insert sample data

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users_test (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO users_test (username) VALUES ('jenkins_user');
+SELECT * FROM users_test;
